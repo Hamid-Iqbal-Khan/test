76a4caa3092947c6a4baeb0176bb43cda914b05f
Hamid-Iqbal-Khan <79803192+Hamid-Iqbal-Khan@users.noreply.github.com>
2024-09-23 14:48:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 # and add decorators to define the appropriate resource locators for them.
 
 @app.route('/')
-@app.route('/hello')
 def hello():
    # Render the page
    return "Hello Python!"
